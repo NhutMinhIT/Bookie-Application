717f510004c314cc5dbb7fc321eacb86d32594ae
NhutMinhIT <nhutminh.it19@gmail.com>
2023-07-29 23:20:11 +0700

    get author by id

--- src/controllers/authorsController.ts
+++ src/controllers/authorsController.ts
@@ -8,8 +8,35 @@ export class AuthorsController {
 
     return res.status(200).json({
       success: true,
-      message: " Fetched authors successfully",
+      message: " Fetched Authors Successfully",
       data: authors,
     });
+  }
+
+  async getAuthorByID(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const author = await AppDataSource.getRepository(Author).findOne({
+        where: { id: Number(id) },
+      });
+
+      if (!author) {
+        return res.status(404).json({
+          success: false,
+          message: "Author does not exist !!!",
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: "Fetch Author",
+        data: author,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Error fetching author.",
+      });
+    }
   }
 }
--- src/routes/authors.ts
+++ src/routes/authors.ts
@@ -6,5 +6,5 @@ const authorsController = new AuthorsController();
 const router = express.Router();
 
 router.get("/", authorsController.getAuthors);
-
+router.get("/author/:id", authorsController.getAuthorByID);
 export default router;
